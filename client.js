7b152f925268c87b2e15b22074d6f19bc1c35189
210050124 <210050124@iitb.ac.in>
2022-11-15 22:54:58 +0530

    Implemented room creating features

--- client.js
+++ client.js
@@ -1,2 +1,24 @@
-console.log("Hello!");
 const socket = io('http://localhost:3000');
+
+// socket.on('serverToClient', (data) =>{
+//     alert(data);
+// })
+
+// socket.emit('clientToServer', "Hello, server!");
+
+function joinRoom(e){
+    
+};
+
+function makeRoom(e){
+    socket.emit('makeRoom', '');
+}
+
+socket.on("newRoomCode", (data) => {
+    document.getElementById("make_room").hidden = true;
+    document.getElementById("join_room").hidden = true;
+    alert(data);
+})
+
+document.getElementById("make_room").addEventListener("click", makeRoom);
+document.getElementById("join_room").addEventListener("click", joinRoom);
--- server.js
+++ server.js
@@ -7,11 +7,46 @@ const io = require('socket.io')(server, {
       origin: '*',
     }
   });
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var roomCodes = {}
+var users = {}
+
+function generateString(length) {
+  let result = ' ';
+  const charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    users[socket.id] = {
+      "room": undefined
+    };
+
+    socket.on('makeRoom', (data) => {
+      if(users[socket.id]["room"] != undefined){
+        console.log("Already in room!");
+        return;
+      }
+
+      let roomCode = ""
+      do{
+        roomCode = generateString(5);
+      }while(roomCodes.hasOwnProperty(roomCode));
+      console.log(roomCode);
+
+      socket.join(roomCode);
+      roomCodes[roomCode] = [socket.id];
+      users[socket.id]["room"] = roomCode;
+
+      console.log(roomCodes);
+      socket.emit("newRoomCode",roomCode);
+    })
   });
