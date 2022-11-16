f32e21fc5eac4613c3604fe36b5eed869f5b0b9a
210050124 <210050124@iitb.ac.in>
2022-11-16 20:34:51 +0530

    Rooms Done

--- client.js
+++ client.js
@@ -1,42 +1,98 @@
 // 'use strict';
-const socket = io('http://localhost:3000');
+const socket = io("http://localhost:3000");
 
-function joinRoom(e){
-    
-};
-
-function makeRoom(e){
-    socket.emit('makeRoom', '');
+function joinRoom(e) {
+	e.preventDefault();
+	var inputRoomCode = document.getElementById("roomCodeInput").value;
+	if (inputRoomCode.length != 5) {
+		alert("Invalid Room Code");
+		return;
+	}
+	socket.emit("joinRoom", inputRoomCode);
 }
 
-function App(props){
-    const [page, changePage] = React.useState("roomButtons");
-
-    socket.on("newRoomCode", (data) => {
-        changePage("waitPlayers");
-        alert(data);
-    })
-
-    if(page == "roomButtons"){
-        return (
-            <div className="main__buttons">
-                <button id="make_room" type="button" onClick={makeRoom} className="btn btn-lg btn-primary m-4">  <i className="bi bi-plus-circle-fill mr-2"/> Make Room</button>
-                
-                <input type="text" className="form-control w-75 mx-auto" placeholder="Room Code"/>
-                <button id="join_room" className="btn btn-lg btn-primary m-4"> <i className="bi bi-box-arrow-in-right mr-2" /> Join Room</button>
-            </div>
-        )
-    }
-
-    if(page == "waitPlayers"){
-        return (
-            <div>
-                Waiting for Players
-            </div>
-        ) 
-    }
+function makeRoom(e) {
+	socket.emit("makeRoom", "");
 }
 
-const rootNode = document.getElementById('root');
+function App(props) {
+	const [page, changePage] = React.useState("roomButtons");
+	const [currentRoomCode, changeRoomCode] = React.useState("");
+	const [numPlayers, changeNumPlayers] = React.useState(1);
+
+	socket.on("newRoomCode", (data) => {
+		changePage("waitPlayers");
+		changeRoomCode(data);
+	});
+
+	socket.on("responseJoinRoom", (data) => {
+		if (!data[0]) {
+			alert("Invalid Room Code");
+			return;
+		} else {
+			changePage("waitPlayers");
+			changeRoomCode(data[1]);
+		}
+	});
+
+	socket.on("numPlayers", (data) => {
+		changeNumPlayers(data);
+	});
+
+	if (page == "roomButtons") {
+		return (
+			<div className="main__buttons">
+				<button
+					id="make_room"
+					type="button"
+					onClick={makeRoom}
+					className="btn btn-lg btn-primary m-4"
+				>
+					{" "}
+					<i className="bi bi-plus-circle-fill mr-2" /> Make Room
+				</button>
+
+                <hr className="hruler"/>
+
+				<form onSubmit={joinRoom}>
+					<input
+						type="text"
+						className="form-control w-75 mx-auto"
+						placeholder="Room Code"
+						id="roomCodeInput"
+					/>
+				</form>
+				<button
+					id="join_room"
+					type="button"
+					className="btn btn-lg btn-primary m-4"
+					onClick={joinRoom}
+				>
+					{" "}
+					<i className="bi bi-box-arrow-in-right mr-2" /> Join Room
+				</button>
+			</div>
+		);
+	}
+
+	if (page == "waitPlayers") {
+		return (
+			<div className="flex wait__main">
+				<div className="wait__players">
+					<div>
+						<img src="images/hourglass.gif" className="mx-5" />
+					</div>
+					<div>
+						<p>Waiting For Players</p>
+						<p>Room Code: {currentRoomCode}</p>
+                        <h1><i class="bi bi-person" size={14}></i> {numPlayers}</h1>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+const rootNode = document.getElementById("root");
 const root = ReactDOM.createRoot(rootNode);
 root.render(React.createElement(App));
--- server.js
+++ server.js
@@ -1,52 +1,90 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const server = app.listen(port)
-const io = require('socket.io')(server, {
-    cors: {
-      origin: '*',
-    }
-  });
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const express = require("express");
+const app = express();
+const port = 3000;
+const server = app.listen(port);
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+	},
+});
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
 
-const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var roomCodes = {}
-var users = {}
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var roomCodes = {};
+var users = {};
 
 function generateString(length) {
-  let result = ' ';
-  const charactersLength = characters.length;
-  for ( let i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
+	let result = "";
+	const charactersLength = characters.length;
+	for (let i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
 
-  return result;
+	return result;
 }
 
-io.on('connection', (socket) => {
-    users[socket.id] = {
-      "room": undefined
-    };
+io.on("connection", (socket) => {
+	users[socket.id] = {
+		room: undefined,
+	};
 
-    socket.on('makeRoom', (data) => {
-      if(users[socket.id]["room"] != undefined){
-        console.log("Already in room!");
-        return;
-      }
+	socket.on("makeRoom", (data) => {
+		if (users[socket.id]["room"] != undefined) {
+			console.log("Already in room!");
+			return;
+		}
 
-      let roomCode = ""
-      do{
-        roomCode = generateString(5);
-      }while(roomCodes.hasOwnProperty(roomCode));
-      console.log(roomCode);
+		let roomCode = "";
+		do {
+			roomCode = generateString(5);
+		} while (roomCodes.hasOwnProperty(roomCode));
+		console.log(roomCode);
 
-      socket.join(roomCode);
-      roomCodes[roomCode] = [socket.id];
-      users[socket.id]["room"] = roomCode;
+		socket.join(roomCode);
+		roomCodes[roomCode] = [socket.id];
+		users[socket.id]["room"] = roomCode;
 
-      console.log(roomCodes);
-      socket.emit("newRoomCode",roomCode);
-    })
-  });
+		console.log(roomCodes);
+		socket.emit("newRoomCode", roomCode);
+	});
+
+	socket.on("joinRoom", (data) => {
+		if (users[socket.id]["room"] != undefined) {
+			console.log("Already in room!");
+			return;
+		}
+
+		if (!(data in roomCodes)) {
+			console.log("Invalid Room Code");
+			socket.emit("responseJoinRoom", [false, ""]);
+			return;
+		}
+
+		socket.join(data);
+		roomCodes[data].push(socket.id);
+		io.to(data).emit("numPlayers", roomCodes[data].length);
+		users[socket.id]["room"] = data;
+		socket.emit("responseJoinRoom", [true, data]);
+	});
+
+	socket.on("disconnect", ()=>{
+		console.log("Bye");
+		userRoomCode = users[socket.id]["room"];
+		delete users[socket.id];
+
+		if(userRoomCode == undefined){
+			return;
+		}
+		
+		const index = roomCodes[userRoomCode].indexOf(socket.id);
+		roomCodes[userRoomCode].splice(index, 1);
+		if(roomCodes[userRoomCode].length == 0){
+			delete roomCodes[userRoomCode];
+			return;
+		}
+
+		io.to(userRoomCode).emit("numPlayers", roomCodes[userRoomCode].length);
+	})
+});
