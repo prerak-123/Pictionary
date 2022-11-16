07d8d256d8e244500679a28d9c07fe46c59b9579
210050124 <210050124@iitb.ac.in>
2022-11-17 03:02:46 +0530

    Whiteboard implemented!

--- client.js
+++ client.js
@@ -1,6 +1,79 @@
 "use strict";
 const socket = io("http://localhost:3000");
 
+//----------------------------------------------------
+
+var mouseDown = 0;
+document.body.onmousedown = function () {
+	++mouseDown;
+};
+document.body.onmouseup = function () {
+	--mouseDown;
+};
+
+function init() {
+	var canvas = document.querySelector("#paint");
+	if (canvas == null) {
+		return;
+	}
+	var ctx = canvas.getContext("2d");
+
+	var sketch = document.querySelector("#sketch");
+	var sketch_style = getComputedStyle(sketch);
+	canvas.width = parseInt(sketch_style.getPropertyValue("width"));
+	canvas.height = parseInt(sketch_style.getPropertyValue("height"));
+
+	var mouse = { x: 0, y: 0 };
+	var last_mouse = { x: 0, y: 0 };
+
+	/* Mouse Capturing Work */
+	canvas.addEventListener(
+		"mousemove",
+		function (e) {
+			last_mouse.x = mouse.x;
+			last_mouse.y = mouse.y;
+
+			mouse.x = e.pageX - this.offsetLeft;
+			mouse.y = e.pageY - this.offsetTop;
+
+			if (!mouseDown) {
+				canvas.removeEventListener("mousemove", onPaint, false);
+			}
+		},
+		false
+	);
+
+	/* Drawing on Paint App */
+	ctx.lineWidth = 5;
+	ctx.lineJoin = "round";
+	ctx.lineCap = "round";
+	ctx.strokeStyle = "black";
+
+	canvas.addEventListener(
+		"mousedown",
+		function (e) {
+			canvas.addEventListener("mousemove", onPaint, false);
+		},
+		false
+	);
+
+	canvas.addEventListener(
+		"mouseup",
+		function () {
+			canvas.removeEventListener("mousemove", onPaint, false);
+		},
+		false
+	);
+
+	var onPaint = function () {
+		ctx.beginPath();
+		ctx.moveTo(last_mouse.x, last_mouse.y);
+		ctx.lineTo(mouse.x, mouse.y);
+		ctx.closePath();
+		ctx.stroke();
+	};
+}
+//----------------------------
 
 function joinRoom(e) {
 	e.preventDefault();
@@ -20,6 +93,10 @@ function App(props) {
 	const [page, changePage] = React.useState("test");
 	const [currentRoomCode, changeRoomCode] = React.useState("");
 	const [numPlayers, changeNumPlayers] = React.useState(1);
+
+	React.useEffect(() => {
+		init();
+	});
 
 	socket.on("newRoomCode", (data) => {
 		changePage("waitPlayers");
@@ -97,9 +174,97 @@ function App(props) {
 
 	if (page == "test") {
 		return (
-			<div>
-				Hi
-			</div>
+			<>
+				<div id="sketch" className="sketch__div">
+					<div className="canvas__buttons">
+						<button
+							className="btn color__button my-1"
+							style={{ backgroundColor: "black" }}
+							onClick={() => {
+								var canvas = document.querySelector("#paint");
+								if (canvas == null) {
+									return;
+								}
+								var ctx = canvas.getContext("2d");
+								ctx.strokeStyle = "black";
+								ctx.lineWidth = 5;
+							}}
+						/>
+
+						<button
+							className="btn color__button my-1"
+							style={{ backgroundColor: "brown" }}
+							onClick={() => {
+								var canvas = document.querySelector("#paint");
+								if (canvas == null) {
+									return;
+								}
+								var ctx = canvas.getContext("2d");
+								ctx.strokeStyle = "brown";
+								ctx.lineWidth = 5;
+							}}
+						/>
+
+						<button
+							className="btn color__button my-1"
+							style={{ backgroundColor: "red" }}
+							onClick={() => {
+								var canvas = document.querySelector("#paint");
+								if (canvas == null) {
+									return;
+								}
+								var ctx = canvas.getContext("2d");
+								ctx.strokeStyle = "red";
+								ctx.lineWidth = 5;
+							}}
+						/>
+
+						<button
+							className="btn color__button my-1"
+							style={{ backgroundColor: "blue" }}
+							onClick={() => {
+								var canvas = document.querySelector("#paint");
+								if (canvas == null) {
+									return;
+								}
+								var ctx = canvas.getContext("2d");
+								ctx.strokeStyle = "blue";
+								ctx.lineWidth = 5;
+							}}
+						/>
+
+						<button
+							className="btn color__button my-1"
+							style={{ backgroundColor: "yellow" }}
+							onClick={() => {
+								var canvas = document.querySelector("#paint");
+								if (canvas == null) {
+									return;
+								}
+								var ctx = canvas.getContext("2d");
+								ctx.strokeStyle = "yellow";
+								ctx.lineWidth = 5;
+							}}
+						/>
+
+						<button
+							className="btn color__button my-1 btn-dark"
+							onClick={() => {
+								var canvas = document.querySelector("#paint");
+								if (canvas == null) {
+									return;
+								}
+								var ctx = canvas.getContext("2d");
+								ctx.strokeStyle = "white";
+								ctx.lineWidth = 30;
+							}}
+						>
+							<h5><i class="bi bi-eraser-fill"></i></h5>
+						</button>
+					</div>
+					<canvas id="paint" className="paint__canvas"></canvas>
+				</div>
+			</>
 		);
 	}
 }
